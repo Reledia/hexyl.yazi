64daf93a67d75eff871befe52d9013687171ffad
Reledia <jacopo778.raineri@gmail.com>
2024-06-15 17:02:26 +0200

    Insert borders when the file is empty

--- init.lua
+++ init.lua
@@ -1,17 +1,29 @@
 local M = {}
 
 function M:peek()
-	local child = Command("hexyl")
-		:args({
-			"--border",
-			"none",
-			"--terminal-width",
-			tostring(self.area.w),
-			tostring(self.file.url),
-		})
-		:stdout(Command.PIPED)
-		:stderr(Command.PIPED)
-		:spawn()
+	local child
+	local l = self.file.cha.length
+	if l == 0 then
+		child = Command("hexyl")
+			:args({
+				tostring(self.file.url),
+			})
+			:stdout(Command.PIPED)
+			:stderr(Command.PIPED)
+			:spawn()
+	else
+		child = Command("hexyl")
+			:args({
+				"--border",
+				"none",
+				"--terminal-width",
+				tostring(self.area.w),
+				tostring(self.file.url),
+			})
+			:stdout(Command.PIPED)
+			:stderr(Command.PIPED)
+			:spawn()
+	end
 
 	local limit = self.area.h
 	local i, lines = 0, ""
